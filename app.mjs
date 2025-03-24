c0b23f31f2e22c1e789d7dbfb5d155c7733210e4
jpdoshi <jpdoshi2811@gmail.com>
2025-03-24 19:09:37 +0530

    added host to work on network

--- app.mjs
+++ app.mjs
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 
-import { PORT, MONGO_URI } from "./config.mjs";
+import { PORT, MONGO_URI, HOST } from "./config.mjs";
 
 import CoffeeRoutes from "./routes/CoffeeRoutes.mjs";
 
@@ -21,7 +21,7 @@ app.use(cors());
 
 app.use("/coffee", CoffeeRoutes);
 
-app.listen(PORT, (err) => {
+app.listen(PORT, HOST, (err) => {
   if (err) console.log(err);
   console.log(`App is listening on PORT:${PORT}`);
 });
--- config.mjs
+++ config.mjs
@@ -2,6 +2,7 @@ import { configDotenv } from "dotenv";
 configDotenv();
 
 export const PORT = process.env.PORT || 8000;
+export const HOST = process.env.HOST || "localhost";
 export const MONGO_URI = `mongodb+srv://jpdoshi2811:${encodeURIComponent(
   process.env.MONGO_PWD
 )}@cluster0.pxzycum.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
